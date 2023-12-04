1921bde5730a77438ee2510215a69ec54af28e3a
Rajeev <rajeevkr8917@gmail.com>
2023-12-04 16:06:13 +0530

    crypto-js added

--- pages/api/signin.js
+++ pages/api/signin.js
@@ -1,12 +1,15 @@
 import User from "@/models/User";
 import connectDb from "@/middleware/mongoose";
+var CryptoJS = require('crypto-js');
 
 const handler = async (req, res) => {
     if (req.method === 'POST') {
         try {
             let user = await User.findOne({ 'email': req.body.email });
+            const bytes = CryptoJS.AES.decrypt(user.password, 'Dream123');
+            const decryptPass = bytes.toString(CryptoJS.enc.Utf8);
             if (user) {
-                if (user.email === req.body.email && user.password === req.body.password) {
+                if (user.email === req.body.email && req.body.password === decryptPass) {
                     res.status(200).json({ success: true, email: user.email, name: user.name });
                 } else {
                     res.status(401).json({ success: false, error: 'Invalid email or password' });
